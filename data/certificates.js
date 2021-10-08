c436c507bd833ae6dc13fc492bf900b80c1bde5d
Eduard Stefan <eduardstefan290@gmail.com>
2021-10-08 23:20:18 +0200

    added certificates data

---
+++ data/certificates.js
@@ -0,0 +1,26 @@
+const certificates = [
+  {
+    title: 'The Web Development Bootcamp',
+    link: 'https://www.udemy.com/certificate/UC-JY8G69HR/',
+    dateIssued: '2019-11-16',
+    desc: '',
+  },
+  {
+    title: 'Responsive Web Design',
+    link: 'https://www.freecodecamp.org/certification/edustef/responsive-web-design',
+    dateIssued: '2019-12-28',
+    desc: '',
+  },
+  {
+    title: 'NodeJS - The Complete Guide',
+    link: 'https://www.udemy.com/certificate/UC-de2cd267-45b9-4bb9-a21e-30d989552a55',
+    dateIssued: '2021-07-21',
+    desc: '',
+  },
+  {
+    title: 'React - The Complete Guide',
+    link: 'https://www.udemy.com/certificate/UC-59caf68b-de30-4557-8b7b-aa06d6c70d49',
+    dateIssued: '2021-08-18',
+    desc: '',
+  },
+];
