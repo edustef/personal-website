7668045685c384e58349ef9b284b1037bb176093
Eduard Stefan <eduardstefan290@gmail.com>
2021-10-26 19:15:09 +0200

    added profile about me

--- components/Description.js
+++ components/Description.js
@@ -3,8 +3,7 @@ import React from 'react'
 export default function Description({ profile }) {
   return (
     <p className='max-w-3xl p-6 mx-auto leading-relaxed tracking-wide text-center border border-gray-300 rounded-md bg-gray-50'>
-      After finishing High School of Arts I quickly transitioned into the programming world. Here I discovered my
-      passion for Frontend I can use both my creativity and logical thinking.
+      {profile.aboutMe}
     </p>
   )
 }
