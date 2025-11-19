cd58a3d0f13c338a3e5ec49b3b22c36c6beebe8c
edustef <eduardstefan290@gmail.com>
2025-11-19 14:18:11 +0200

    Add PostHog integration and update Next.js configuration
    - Introduced PostHog for analytics with a new instrumentation client.
    - Updated Next.js configuration to include rewrites for PostHog assets.
    - Added PostHog dependency to package.json and updated package-lock.json for consistency.
    - Adjusted tsconfig.json to include the new instrumentation client file.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/magical-app/static/:path*",
+        destination: "https://eu-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/magical-app/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
   env: {
     // Matches the behavior of `sanity dev` which sets styled-components to use the fastest way of inserting CSS rules in both dev and production. It's default behavior is to disable it in dev mode.
     SC_DISABLE_SPEEDY: "false",
