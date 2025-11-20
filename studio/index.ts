548a255722d167e4cc9dd103eeb79f6c7e1aaa09
edustef <eduardstefan290@gmail.com>
2025-11-20 14:43:11 +0200

    refactor: update import paths and schemas in index.ts for improved organization

--- studio/index.ts
+++ studio/index.ts
@@ -1,18 +1,17 @@
-import certificates from "schemas/documents/certificates";
-import job from "schemas/documents/job";
-import page from "schemas/documents/page";
-import project from "schemas/documents/project";
-import skill from "schemas/documents/skill";
-import button from "schemas/objects/button";
-import duration from "schemas/objects/duration";
-import milestone from "schemas/objects/milestone";
-import socialLink from "schemas/objects/socialLink";
-import timeline from "schemas/objects/timeline";
-import home from "schemas/singletons/home";
-import profile from "schemas/singletons/profile";
-import settings from "schemas/singletons/settings";
+import { certificate } from "./src/schemaTypes/documents/certificates";
+import { job } from "./src/schemaTypes/documents/job";
+import { project } from "./src/schemaTypes/documents/project";
+import { skill } from "./src/schemaTypes/documents/skill";
+import { button } from "./src/schemaTypes/objects/button";
+import { duration } from "./src/schemaTypes/objects/duration";
+import { milestone } from "./src/schemaTypes/objects/milestone";
+import { socialLink } from "./src/schemaTypes/objects/socialLink";
+import { timeline } from "./src/schemaTypes/objects/timeline";
+import { home } from "./src/schemaTypes/singletons/home";
+import { profile } from "./src/schemaTypes/singletons/profile";
+import { settings } from "./src/schemaTypes/singletons/settings";
 
-export const documentsSchemas = [page, project, skill, job, certificates];
+export const documentsSchemas = [certificate, job, project, skill];
 
 export const singletonsSchemas = [home, settings, profile];
 
@@ -24,4 +23,4 @@ export const objectsSchemas = [
   socialLink,
 ];
 
-export const PREVIEWABLE_DOCUMENT_TYPES: string[] = [home.name, page.name];
+export const PREVIEWABLE_DOCUMENT_TYPES: string[] = [home.name];
